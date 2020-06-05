b9abc9b6dcdc4ff854f0f462673cf91ebd256913
Kim Le <kimle.lek@gmail.com>
2020-06-05 18:39:08 -0400

    changes to the layout of the readme.md

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
- # scheduler
+ # Scheduler
+[![GitHub license](https://img.shields.io/badge/license-ISC-blue)](https://github.com/kimnana45/scheduler)
 
-    [![GitHub license](https://img.shields.io/badge/license-ISC-blue)](https://github.com/kimnana45/scheduler)
-
-    ## Description 
-    a daily schedule maker
+## Description 
+A daily planner to help you organize your todo list.
 
     ## User Story
     ```
-    AS A student
-    I WANT to be organize
-    SO THAT to finidh all things 
+    AS A STUDENT
+    I WANT TO bE Organize
+    SO THAT I can finish all my todo list.
     ```
 
     ## Table of Content 
@@ -24,11 +23,11 @@
     ```
 
     ## Usage
-    have fun
+    Be mindful!
 
     ## License
     ISC
 
     ## Questions
-    If you have any questions about the repo, please contact kimnana45 at kim@email.com.
+    If you have any questions about the repo, please contact kimnnana45 at kimle.lek@gmail.com.
     
--- questions.js
+++ questions.js
@@ -3,42 +3,50 @@ module.exports = {
         {
             type: "input",
             name: "username",
-            message: "What is your GitHub username?"
+            message: "What is your GitHub username?",
+            default: "kimnnana45",
         },
         {
             type: "input",
             name: "email",
-            message: "What is your email?"
+            message: "What is your email?",
+            default: "kimle.lek@gmail.com"
         },
         {
             type: "input",
             name: "url",
-            message: "the URL to your project?"
+            message: "the URL to your project?",
+            default: "https://github.com/kimnana45/scheduler",
         },
         {
             type: "input",
             name: "name",
-            message: "What is your project's name?"
+            message: "What is your project's name?",
+            default: "Scheduler",
         },
         {
             type: "input",
             name: "description",
-            message: "Please write a short description of your project"
+            message: "Please write a short description of your project",
+            default: "A daily planner to help you organize your todo list."
         },
         {
             type: "input",
             name: "role",
-            message: "What is the user's role in your project's user story? (AS A ...)"
+            message: "What is the user's role in your project's user story? (AS A ...)",
+            default: "STUDENT"
         },
         {
             type: "input",
             name: "goal",
-            message: "What is the goal for your project user's story?(I WANT ...)"
+            message: "What is the goal for your project user's story?(I WANT ...)",
+            default: "TO bE Organize",
         },
         {
             type: "input",
             name: "reason",
-            message: "What is the reason for your project's user story?(SO THAT ...)"
+            message: "What is the reason for your project's user story?(SO THAT ...)",
+            default: "I can finish all my todo list."
         },
         {
             type: "list",
@@ -56,6 +64,7 @@ module.exports = {
             type: "input",
             name: "usage",
             message: "What does the user need to know about using the repo?",
+            default: "Be mindful!",
         },
     ]
 }
